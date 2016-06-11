391f5e7b96ae3e99fcedd11c4a9a254da19c5a42
Hugo <marcellinhugo@orange.fr>
2016-06-11 22:02:46 +0200

    Avec un menu déroulant ça fait mieux

--- resources/views/template_base.blade.php
+++ resources/views/template_base.blade.php
@@ -43,7 +43,12 @@
             <li><a href="#">Administration</a></li>
           @endif
           @if (Auth::check())
-          <li><a href={{URL('/publication/choosetype')}} >Nouvelle publication</a></li>
+          <li class="dropdown">
+               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Publications <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href={{URL('/publication/choosetype')}} >Nouvelle publication</a></li>
+                </ul>
+          </li>
           @endif
         @endunless
       </ul>
